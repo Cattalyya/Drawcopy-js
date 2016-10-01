1411301cee2fe8231660d1cd64f5bfabf9972c08
Cattalyya Nuengsigkapian <KitCat@dhcp-18-189-97-193.dyn.mit.edu>
2016-09-30 21:54:49 -0400

    Add painting canvas

---
+++ js/controllers/painting.js
@@ -0,0 +1,9 @@
+app.controller('PaintingCtrl',function($scope, sharedDataService, eventListenerService){
+
+    var sketchpad = new Sketchpad({
+      element: '#sketchpad',
+      width: 400,
+      height: 400,
+    });
+
+});
